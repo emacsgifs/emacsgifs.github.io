c0a93b4178c3b41368c68a52f52d7748b8d06add
jasonm23 <jasonm23@gmail.com>
2016-08-06 23:32:09 +0800

    fix index template

--- new-tweets/template/tweet.index.template.rb
+++ new-tweets/template/tweet.index.template.rb
@@ -4,5 +4,5 @@ tweet_id=ENV['tweet_id']
 body=ENV['body']
 
 puts %Q[<tr><td class="emacs-gif-archive-entry">
-  <a href="/tweets/#{tweet_id}"> #{body}</a>
+  <a href="/tweets/#{tweet_id}.html"> #{body}</a>
 </td></tr>]
